bf20a77cb7baefd391d25d9168aa7574ca976958
Haz <master@hazonline.co.uk>
2018-03-15 23:58:12 +0000

    Add files via upload

---
+++ PiCal/PiCal/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace PiCal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startworker = new System.Windows.Forms.Button();
+            this.pi_box = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // startworker
+            // 
+            this.startworker.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.startworker.Location = new System.Drawing.Point(0, 207);
+            this.startworker.Name = "startworker";
+            this.startworker.Size = new System.Drawing.Size(284, 54);
+            this.startworker.TabIndex = 0;
+            this.startworker.Text = "Run";
+            this.startworker.UseVisualStyleBackColor = true;
+            this.startworker.Click += new System.EventHandler(this.startworker_Click);
+            // 
+            // pi_box
+            // 
+            this.pi_box.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pi_box.Location = new System.Drawing.Point(0, 0);
+            this.pi_box.Multiline = true;
+            this.pi_box.Name = "pi_box";
+            this.pi_box.Size = new System.Drawing.Size(284, 207);
+            this.pi_box.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pi_box);
+            this.Controls.Add(this.startworker);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startworker;
+        private System.Windows.Forms.TextBox pi_box;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PiCal/PiCal/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+
+namespace PiCal
+{
+    public partial class Form1 : Form
+    {
+        public Random _ran;
+        public Form1()
+        {
+            InitializeComponent();
+            _ran = new Random();
+             pi_t= new Thread(new ThreadStart(pi_calto));
+        }
+        public int radious = 10000;
+        public decimal pi_f = 0;
+        public Thread pi_t;
+        public bool notstart = true;
+        public bool paused = false;
+        private void startworker_Click(object sender, EventArgs e)
+        {
+            if (notstart)
+            {
+                notstart = false;
+                pi_t.Start();
+            }else
+            if (paused)
+            {
+                pi_t.Resume();
+                paused = false;
+
+            }
+            else
+            {
+                pi_t.Suspend();
+                paused = true;
+            }
+            
+            //pi_calto();
+        }
+        public ulong count = 0;
+        public ulong ccount = 0;
+        public void pi_calto()
+        {
+            while (true)
+            {
+                for (int i = 0; i < 500000; i++)
+                {
+                    decimal x = _ran.Next(-radious, radious);
+                    decimal y = _ran.Next(-radious, radious);
+                    count += 1;
+                    //MessageBox.Show(count.ToString());
+                    decimal mag = (x * x + y * y);
+                    if (mag < radious * radious)
+                    {
+                        ccount += 1;
+                    }
+                }
+                //MessageBox.Show(count.ToString()+" / "+ccount.ToString());
+                pi_f = 4 * ((decimal)ccount / (decimal)count);
+                
+                //pi_box.Text = pi_f.ToString();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(pi_f != 0)
+            {
+                //MessageBox.Show(pi_f.ToString());
+                pi_box.Text = pi_f.ToString();
+            }
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            pi_t.Abort();
+        }
+    }
+}
